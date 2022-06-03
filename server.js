06070c1e219b01b45bd707d883523b336cc74c3b
Jesus G Jara <jesusgjara.1@gmail.com>
2022-06-02 20:14:30 -0400

    Added index.html, and server.js. Setup Server

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const app = express()
+const PORT = 8000
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}`)
+})
