5a9755dbb737ab128965d86f3a220e535f56870d
Jesus G Jara <jesusgjara.1@gmail.com>
2022-06-02 20:31:59 -0400

    Create rappers database

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Shéya Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennet',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+
+    
+}
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api:savage', (req, res) => {
+    res.json(savage)
 })
 
 app.listen(PORT, () => {
